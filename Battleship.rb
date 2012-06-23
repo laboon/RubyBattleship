ba287cec39c69691bfe3bc4f9541bf2595f64ac7
Bill Laboon <laboon@gmail.com>
2012-06-23 12:55:43 -0400

    Ships moved to separate file

--- Battleship.rb
+++ Battleship.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/ruby
 
+# Referenced files
+require 'Ships'
+# require 'Board'
+
 # Constants
 EmptySquare = "."
 ShipSquare ="+"
@@ -211,72 +215,6 @@ class Enemy
     x, y = rand(10), rand(10)
     return x, y
   end
-end
-
-class Ship
-  def initialize(size, orientation, x, y)
-    @size = size
-    @orientation = orientation
-    @x = x
-    @y = y
-    
-    @rep = ShipSquare
-    @name = "Undefined"
-  end
-  
-  attr_reader :size, :orientation, :x, :y, :rep, :name
-  
-end
-
-#
-class AircraftCarrier < Ship
-  Size = 5
-  def initialize(orientation, x, y)
-    super(Size, orientation, x, y)
-    @rep = ACSquare
-    @name = "Aircraft Carrier"
-  end
-  
-end
-
-class Battleship < Ship
-  Size = 4
-  def initialize(orientation, x, y)
-    super(Size, orientation, x, y)
-    @rep = BSquare
-    @name = "Battleship"
-  end
-  
-end
-
-class Submarine < Ship
-  Size = 3
-  def initialize(orientation, x, y)
-    super(Size, orientation, x, y)
-    @rep = SubSquare
-    @name = "Submarine"
-  end
-  
-end
-
-class Cruiser < Ship
-  Size = 3
-  def initialize(orientation, x, y)
-    super(Size, orientation, x, y)
-    @rep = CruiserSquare
-    @name = "Cruiser"
-  end
-  
-end
-
-class Destroyer < Ship
-  Size = 2
-  def initialize(orientation, x, y)
-    super(Size, orientation, x, y)
-    @rep = DestroyerSquare
-    @name = "Destroyer"
-  end
-  
 end
 
 def checkForWin
